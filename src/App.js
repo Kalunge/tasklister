861586bd7d3b28d5988666c527e7546dcb0a0b58
kalunge <muthomititus+github@gmail.com>
2022-06-27 16:51:08 +0300

    Toggle reminder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
+import { useState } from 'react';
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Wife's birthday",
+      day: 'February 4th at 8:30 am',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Hubby's birthday",
+      day: 'April 24th at 8:30 am',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Son's birthday",
+      day: 'July 1st at 8:30 am',
+      reminder: true,
+    },
+  ]);
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task,
+      ),
+    );
+  };
   return (
     <div className="container">
-      <Header name="Titus Muthomi"/>
+      <Header name="Titus Muthomi" />
+      {tasks.length ? (
+        <Tasks
+          tasks={tasks}
+          onDelete={deleteTask}
+          onDoubleTap={toggleReminder}
+        />
+      ) : (
+        'No tasks'
+      )}
     </div>
   );
 };
